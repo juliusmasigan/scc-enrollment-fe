c1be8813f288aea1ee6adae899a94809548bb46c
Julius L. Masigan <juliusmasigan@gmail.com>
2015-10-28 07:49:48 +0800

    Initial commit.

---
+++ Gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var server = require('gulp-server-livereload');
+
+gulp.task('styles', function() {
+    return gulp.src('./bower_components/**/*.min.css')
+        .pipe(concat('styles.min.css'))
+        .pipe(gulp.dest('./app/css/'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('./bower_components/**/*.min.js')
+        .pipe(concat('scripts.min.js'))
+        .pipe(gulp.dest('./app/js/'));
+});
+
+gulp.task('serve', function() {
+    gulp.src('app')
+        .pipe(server({
+            livereload: true,
+            directoryFile: './app/index.html',
+            open: true
+        }));
+});
+
+gulp.task('default', ['styles', 'scripts', 'serve']);
